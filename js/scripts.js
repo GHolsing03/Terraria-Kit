63ccd3f9472509546e3c12f709877a313f7fb8d2
GHolsing03 <rholsing03@gmail.com>
2025-07-19 21:56:21 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
-const tabs = document.querySelectorAll('.tab');
-const contents = document.querySelectorAll('.content');
+document.getElementById("fishingTabContent").style.display = "block";
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        contents.forEach(c => c.classList.remove('active'));
-        const contentId = tab.getAttribute('data-content');
-        document.getElementById(contentId).classList.add('active');
-    });
-});
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active","");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
